c86cd7553af39dc26c428e9b8d7b79d888e8c479
edgar <edgar.riba@gmail.com>
2025-04-06 17:33:46 +0200

    unify broadcast task

--- src/cu29/tasks/broadcast.rs
+++ src/cu29/tasks/broadcast.rs
@@ -7,94 +7,72 @@ use cu29::prelude::*;
 use kornia::io::jpeg::ImageEncoder;
 use std::time::Duration;
 
-pub struct BroadcastImage {
-    jpeg_encoder: ImageEncoder,
+pub struct Broadcast {
+    image_encoder: ImageEncoder,
 }
 
-impl Freezable for BroadcastImage {}
+impl Freezable for Broadcast {}
 
-impl<'cl> CuSinkTask<'cl> for BroadcastImage {
-    type Input = input_msg!('cl, ImageRgb8Msg);
+impl<'cl> CuSinkTask<'cl> for Broadcast {
+    type Input = input_msg!('cl, ImageRgb8Msg, PromptResponseMsg);
 
     fn new(_config: Option<&ComponentConfig>) -> Result<Self, CuError>
     where
         Self: Sized,
     {
         Ok(Self {
-            jpeg_encoder: ImageEncoder::new()
+            image_encoder: ImageEncoder::new()
                 .map_err(|e| CuError::new_with_cause("Failed to create jpeg encoder", e))?,
         })
     }
 
     fn process(&mut self, _clock: &RobotClock, input: Self::Input) -> Result<(), CuError> {
-        let Some(img_msg) = input.payload() else {
-            return Ok(());
-        };
+        let (img_msg, prompt_msg) = input;
 
-        // encode the image to jpeg before broadcasting
-        let encoded_image = self
-            .jpeg_encoder
-            .encode(img_msg)
-            .map_err(|e| CuError::new_with_cause("Failed to encode image", e))?;
+        // broadcast the image
+        if let Some(img) = img_msg.payload() {
+            let encoded_image = self
+                .image_encoder
+                .encode(img)
+                .map_err(|e| CuError::new_with_cause("Failed to encode image", e))?;
 
-        // get the acquisition time of the image
-        let acq_time: Duration = match input.metadata.tov {
-            Tov::Time(time) => time.into(),
-            _ => Duration::from_secs(0),
-        };
+            // get the acquisition time of the image
+            let acq_time: Duration = match img_msg.metadata.tov {
+                Tov::Time(time) => time.into(),
+                _ => Duration::from_secs(0),
+            };
 
-        // send the camera image to the global state
-        let _ = SERVER_GLOBAL_STATE
-            .result_store
-            .image
-            .tx
-            .send(CameraResult {
-                timestamp_nanos: acq_time.as_nanos() as u64,
-                image: EncodedImage {
-                    data: encoded_image,
-                    encoding: "jpeg".to_string(),
-                },
-            });
+            // send the camera image to the global state
+            let _ = SERVER_GLOBAL_STATE
+                .result_store
+                .image
+                .tx
+                .send(CameraResult {
+                    timestamp_nanos: acq_time.as_nanos() as u64,
+                    image: EncodedImage {
+                        data: encoded_image,
+                        encoding: "jpeg".to_string(),
+                    },
+                });
+        }
 
-        Ok(())
-    }
-}
+        // broadcast the prompt response
+        if let Some(prompt) = prompt_msg.payload() {
+            let acq_time: Duration = match prompt_msg.metadata.tov {
+                Tov::Time(time) => time.into(),
+                _ => Duration::from_secs(0),
+            };
 
-pub struct BroadcastChat;
-
-impl Freezable for BroadcastChat {}
-
-impl<'cl> CuSinkTask<'cl> for BroadcastChat {
-    type Input = input_msg!('cl, PromptResponseMsg);
-
-    fn new(_config: Option<&ComponentConfig>) -> Result<Self, CuError>
-    where
-        Self: Sized,
-    {
-        Ok(Self {})
-    }
-
-    fn process(&mut self, _clock: &RobotClock, input: Self::Input) -> Result<(), CuError> {
-        let Some(chat_msg) = input.payload() else {
-            return Ok(());
-        };
-
-        // get the timestamp of the inference result
-        let acq_time: Duration = match input.metadata.tov {
-            Tov::Time(time) => time.into(),
-            _ => Duration::from_secs(0),
-        };
-
-        // send the chat result to the global state
-        let _ = SERVER_GLOBAL_STATE
-            .result_store
-            .inference
-            .tx
-            .send(InferenceResult {
-                timestamp_nanos: acq_time.as_nanos() as u64,
-                prompt: chat_msg.prompt.clone(),
-                response: chat_msg.response.clone(),
-            });
+            let _ = SERVER_GLOBAL_STATE
+                .result_store
+                .inference
+                .tx
+                .send(InferenceResult {
+                    timestamp_nanos: acq_time.as_nanos() as u64,
+                    prompt: prompt.prompt.clone(),
+                    response: prompt.response.clone(),
+                });
+        }
 
         Ok(())
     }
