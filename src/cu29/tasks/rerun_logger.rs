2ff5674017438d67b1f4d4fda7fea56e7fb962f3
edgar <edgar.riba@gmail.com>
2025-04-10 21:21:27 +0200

    log images as jpeg encoded

--- src/cu29/tasks/rerun_logger.rs
+++ src/cu29/tasks/rerun_logger.rs
@@ -1,18 +1,15 @@
-use std::net::SocketAddr;
-use std::str::FromStr;
-
-use crate::cu29::msgs::ImageRgb8Msg;
+use crate::cu29::msgs::{EncodedImage, ImageRgb8Msg};
 use cu29::prelude::*;
+use std::path::PathBuf;
 
 pub struct RerunLogger {
-    rec_stats: rerun::RecordingStream,
-    rec_viz: Option<rerun::RecordingStream>,
+    rec: rerun::RecordingStream,
 }
 
 impl Freezable for RerunLogger {}
 
 impl<'cl> CuSinkTask<'cl> for RerunLogger {
-    type Input = input_msg!('cl, ImageRgb8Msg);
+    type Input = input_msg!('cl, EncodedImage);
 
     fn new(config: Option<&ComponentConfig>) -> Result<Self, CuError>
     where
@@ -20,57 +17,33 @@ impl<'cl> CuSinkTask<'cl> for RerunLogger {
     {
         let config = config.expect("config is required");
 
-        // stream for local recording
+        // create the path to the rerun file
         let path = config.get::<String>("path").expect("path is required");
         let rec_path = {
             let timestamp = std::time::SystemTime::now()
                 .duration_since(std::time::UNIX_EPOCH)
                 .unwrap()
                 .as_secs();
-            format!("{}/{}.rrd", path, timestamp)
+            PathBuf::from(path).join(format!("{}.rrd", timestamp))
         };
 
-        let rec_stats = rerun::RecordingStreamBuilder::new("bubbaloop_stats")
+        let rec = rerun::RecordingStreamBuilder::new("rerun_logger")
             .save(rec_path)
             .map_err(|e| CuError::new_with_cause("Failed to spawn rerun stream", e))?;
 
-        // create the remote rerun server stream
-        let rec_viz = match (config.get::<String>("ip"), config.get::<u32>("port")) {
-            (Some(ip), Some(port)) => {
-                let addr = SocketAddr::from_str(&format!("{}:{}", ip, port))
-                    .map_err(|e| CuError::new_with_cause("Failed to parse socket address", e))?;
-
-                // stream for remote visualization
-                Some(
-                    rerun::RecordingStreamBuilder::new("bubbaloop")
-                        .connect_tcp_opts(addr, None)
-                        .map_err(|e| CuError::new_with_cause("Failed to spawn rerun stream", e))?,
-                )
-            }
-            _ => None,
-        };
-
-        Ok(Self { rec_viz, rec_stats })
+        Ok(Self { rec })
     }
 
     fn process(&mut self, _clock: &RobotClock, input: Self::Input) -> Result<(), CuError> {
-        let Some(image) = input.payload() else {
-            return Ok(());
-        };
-
-        // log the image to the local rerun server
-        log_image_rgb(&self.rec_stats, "image", image)?;
-
-        // log the image to the remote rerun server if it is connected
-        if let Some(rec_viz) = &self.rec_viz {
-            log_image_rgb(rec_viz, "image", image)?;
+        if let Some(image) = input.payload() {
+            // log the image to the local rerun server
+            log_image_encoded(&self.rec, "image", image)?;
         }
-
         Ok(())
     }
 }
 
-fn log_image_rgb(
+fn _log_image_rgb8(
     rec: &rerun::RecordingStream,
     name: &str,
     img: &ImageRgb8Msg,
@@ -78,6 +51,19 @@ fn log_image_rgb(
     rec.log(
         name,
         &rerun::Image::from_elements(img.as_slice(), img.size().into(), rerun::ColorModel::RGB),
+    )
+    .map_err(|e| CuError::new_with_cause("Failed to log image", e))?;
+    Ok(())
+}
+
+fn log_image_encoded(
+    rec: &rerun::RecordingStream,
+    name: &str,
+    img: &EncodedImage,
+) -> Result<(), CuError> {
+    rec.log(
+        name,
+        &rerun::EncodedImage::from_file_contents(img.data.clone()),
     )
     .map_err(|e| CuError::new_with_cause("Failed to log image", e))?;
     Ok(())
