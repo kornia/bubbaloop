cab28320698c9d5a5245473b7da94e6364791c9a
edgar <edgar.riba@gmail.com>
2025-05-18 16:56:58 +0200

    upda kornia 0.1.9

--- examples/react-app/src/App.tsx
+++ examples/react-app/src/App.tsx
@@ -1,29 +1,46 @@
 // App.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import StreamViewerWebsocket from './components/StreamViewerWebsocket';
 import InferenceResultDisplay from './components/InferenceDisplay';
 import InferenceInstruction from './components/InferenceInstruction';
+import ConnectionSettings from './components/ConnectionSettings';
 
 const App: React.FC = () => {
+  const [host, setHost] = useState('0.0.0.0');
+  const [port, setPort] = useState('3000');
+  const [key, setKey] = useState(0);
+
+  const baseUrl = `http://${host}:${port}`;
+  const wsUrl = `ws://${host}:${port}`;
+
+  const handleConnectionUpdate = (newHost: string, newPort: string) => {
+    setHost(newHost);
+    setPort(newPort);
+    // Increment key to force re-mounting the components
+    setKey(prevKey => prevKey + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>Bubbaloop</h2>
       </header>
 
-      <main>
+      <ConnectionSettings onUpdate={handleConnectionUpdate} initialHost={host} initialPort={port} />
+
+      <main key={key}>
         <StreamViewerWebsocket
-          wsUrl="ws://0.0.0.0:3000/api/v0/streaming/ws/0"
+          wsUrl={`${wsUrl}/api/v0/streaming/ws/0`}
           maxHeight="500px"
         />
         <InferenceInstruction
-          settingsUrl="http://0.0.0.0:3000/api/v0/inference/settings"
+          settingsUrl={`${baseUrl}/api/v0/inference/settings`}
           placeholder="cap en"
           buttonText="Apply"
         />
         <InferenceResultDisplay
-          inferenceUrl="http://0.0.0.0:3000/api/v0/inference/result"
+          inferenceUrl={`${baseUrl}/api/v0/inference/result`}
           refreshRate={1000}
           width="500px"
           height="500px"
