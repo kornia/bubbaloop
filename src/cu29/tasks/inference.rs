ba29fcdd84dd71f40e842c10db6236929efdb086
Edgar Riba <edgar.riba@gmail.com>
2025-05-25 21:15:50 +0200

    use kornia-infernum as inference server (#38)
    * implement react app example
    
    * cleanup artifacts
    
    * remove vite svg
    
    * cleanup code
    
    * upda kornia 0.1.9
    
    * update readme
    
    * simplify kornia dependencies
    
    * shorten readme
    
    * use kornia-infernum as inference server
    
    * Update README.md

--- src/cu29/tasks/inference.rs
+++ src/cu29/tasks/inference.rs
@@ -3,15 +3,11 @@ use crate::{
     pipeline::SERVER_GLOBAL_STATE,
 };
 use cu29::prelude::*;
-use kornia_paligemma::{Paligemma, PaligemmaConfig, PaligemmaError};
-use std::{
-    sync::{
-        atomic::AtomicBool,
-        mpsc::{Receiver, Sender},
-        Arc, Mutex,
-    },
-    thread::JoinHandle,
+use kornia_infernum::{
+    engine::{InfernumEngine, InfernumEngineRequest, InfernumEngineResult, InfernumEngineState},
+    model::{InfernumModel, InfernumModelRequest, InfernumModelResponse},
 };
+use kornia_paligemma::{Paligemma, PaligemmaConfig, PaligemmaError};
 
 /// The default prompt to use if no prompt is provided
 // NOTE: check the original prompt instructions
@@ -21,7 +17,7 @@ const DEFAULT_PROMPT: &str = "cap en\n";
 /// Task that runs inference on an image
 pub struct Inference {
     current_prompt: String,
-    scheduler: InferenceScheduler,
+    engine: InfernumEngine<PaligemmaModel>,
 }
 
 impl Freezable for Inference {}
@@ -34,18 +30,20 @@ impl<'cl> CuTask<'cl> for Inference {
     where
         Self: Sized,
     {
-        let scheduler = InferenceScheduler::new()
-            .map_err(|e| CuError::new_with_cause("Failed to create Paligemma scheduler", e))?;
+        let paligemma = Paligemma::new(PaligemmaConfig::default())
+            .map_err(|e| CuError::new_with_cause("Failed to create Paligemma", e))?;
+
+        let engine = InfernumEngine::new(PaligemmaModel(paligemma));
 
         Ok(Self {
             current_prompt: DEFAULT_PROMPT.to_string(),
-            scheduler,
+            engine,
         })
     }
 
     fn process(
         &mut self,
-        clock: &RobotClock,
+        _clock: &RobotClock,
         input: Self::Input,
         output: Self::Output,
     ) -> Result<(), CuError> {
@@ -58,7 +56,7 @@ impl<'cl> CuTask<'cl> for Inference {
             .inference_settings
             .rx
             .lock()
-            .unwrap()
+            .expect("Failed to lock inference settings")
             .try_recv()
         {
             log::debug!("Updating prompt to: {}", prompt);
@@ -66,24 +64,24 @@ impl<'cl> CuTask<'cl> for Inference {
         }
 
         // check if we are already processing an inference to not block the main thread
-        if self.scheduler.is_processing() {
+        if self.engine.state() == InfernumEngineState::Processing {
             return Ok(());
         }
 
         // check first if we have a response from the previous inference
-        if let Some((channel_id, prompt, response)) = self.scheduler.try_poll_response() {
+        if let InfernumEngineResult::Success(response) = self.engine.try_poll_response() {
             log::debug!(
                 "Received response from inference thread for channel: {} -- prompt: {} -- response: {}",
-                channel_id,
-                prompt,
-                response
+                response.id,
+                response.prompt,
+                response.response
             );
 
             output.set_payload(PromptResponseMsg {
-                stamp_ns: clock.now().as_nanos(),
-                channel_id,
-                prompt,
-                response,
+                stamp_ns: response.duration.as_nanos() as u64,
+                channel_id: response.id,
+                prompt: response.prompt,
+                response: response.response,
             });
         }
 
@@ -93,99 +91,27 @@ impl<'cl> CuTask<'cl> for Inference {
         };
 
         // send the request to the thread to schedule the inference
-        self.scheduler.schedule_inference(img, &self.current_prompt);
+        self.engine.schedule_inference(InfernumEngineRequest {
+            id: img.channel_id,
+            prompt: self.current_prompt.clone(),
+            image: img.image.clone(),
+        });
 
         Ok(())
     }
 }
 
-struct InferenceScheduler {
-    is_processing: Arc<Mutex<AtomicBool>>,
-    req_tx: Option<Sender<(ImageRgb8Msg, String)>>,
-    rep_rx: Receiver<(u8, String, String)>,
-    inference_handle: Option<JoinHandle<Result<(), PaligemmaError>>>,
-}
+/// Model that uses Paligemma to run inference
+struct PaligemmaModel(Paligemma);
 
-impl InferenceScheduler {
-    pub fn new() -> Result<Self, PaligemmaError> {
-        // NOTE: in future should be able to schedule multiple models
-        let mut paligemma = Paligemma::new(PaligemmaConfig::default())?;
+impl InfernumModel for PaligemmaModel {
+    type Error = PaligemmaError;
 
-        let (req_tx, req_rx) = std::sync::mpsc::channel::<(ImageRgb8Msg, String)>();
-        let (rep_tx, rep_rx) = std::sync::mpsc::channel::<(u8, String, String)>();
+    fn run(&mut self, request: InfernumModelRequest) -> Result<InfernumModelResponse, Self::Error> {
+        let response = self
+            .0
+            .inference(&request.image, &request.prompt, 50, false)?;
 
-        let is_processing = Arc::new(Mutex::new(AtomicBool::new(false)));
-
-        let inference_handle = std::thread::spawn({
-            let is_processing = is_processing.clone();
-            move || -> Result<(), PaligemmaError> {
-                // block the thread until the inference is stopped
-                while let Ok((img_msg, prompt)) = req_rx.recv() {
-                    log::trace!("Scheduling a new inference");
-
-                    let response = paligemma.inference(&img_msg.image, &prompt, 50, false)?;
-
-                    log::trace!("Inference completed");
-
-                    let _ = rep_tx.send((img_msg.channel_id, prompt, response));
-                    is_processing
-                        .lock()
-                        .unwrap()
-                        .store(false, std::sync::atomic::Ordering::Relaxed);
-                }
-                Ok(())
-            }
-        });
-
-        Ok(Self {
-            is_processing,
-            req_tx: Some(req_tx),
-            rep_rx,
-            inference_handle: Some(inference_handle),
-        })
-    }
-
-    pub fn is_processing(&self) -> bool {
-        self.is_processing
-            .lock()
-            .unwrap()
-            .load(std::sync::atomic::Ordering::Relaxed)
-    }
-
-    pub fn try_poll_response(&self) -> Option<(u8, String, String)> {
-        self.rep_rx.try_recv().ok()
-    }
-
-    pub fn schedule_inference(&self, img: &ImageRgb8Msg, prompt: &str) {
-        // SAFETY: we are created the channel in the constructor
-        // TODO: verify that we are not doing a deep copy of the image
-        let _ = self
-            .req_tx
-            .as_ref()
-            .unwrap()
-            .send((img.clone(), prompt.to_string()));
-        // set the processing flag to true as we are scheduling an inference
-        self.is_processing
-            .lock()
-            .unwrap()
-            .store(true, std::sync::atomic::Ordering::Relaxed);
-    }
-
-    pub fn stop(&mut self) {
-        // take ownership of the request channel and close it
-        drop(self.req_tx.take());
-
-        // join the inference thread
-        if let Some(handle) = self.inference_handle.take() {
-            if let Err(_e) = handle.join() {
-                log::error!("Failed to join inference thread");
-            }
-        }
-    }
-}
-
-impl Drop for InferenceScheduler {
-    fn drop(&mut self) {
-        self.stop();
+        Ok(InfernumModelResponse { response })
     }
 }
