1ed3d0f3413a4b68940499f3a0bbbb21939ad848
edgarriba <edgar.riba@gmail.com>
2025-12-21 01:35:53 +0100

    fix gstreamer path

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -3,5 +3,7 @@
 
 export PKG_CONFIG_PATH="$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH"
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
-export GST_PLUGIN_PATH="$CONDA_PREFIX/lib/gstreamer-1.0:$GST_PLUGIN_PATH"
+
+# Include system GStreamer plugins for NVIDIA hardware decoders (nvv4l2decoder, nvvidconv, etc.)
+export GST_PLUGIN_PATH="/usr/lib/aarch64-linux-gnu/gstreamer-1.0:$CONDA_PREFIX/lib/gstreamer-1.0:$GST_PLUGIN_PATH"
 
